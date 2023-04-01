cc3553e8946be803a426d54ea459f16c1071f599
jkubas <116137526+jkubas@users.noreply.github.com>
2023-04-01 19:01:07 +0300

    added

--- setup.py
+++ setup.py
@@ -5,9 +5,18 @@ setup(
     version='1.0.0',
     packages=find_packages(),
     install_requires=[
-          'cryptography',
-        'ed25519',
-        'zeroconf',
+async-timeout==4.0.2
+base36==0.1.1
+cffi==1.15.1
+chacha20poly1305-reuseable==0.0.4
+cryptography==40.0.1
+ed25519==1.5
+h11==0.14.0
+ifaddr==0.2.0
+orjson==3.8.9
+pycparser==2.21
+PyQRCode==1.2.1
+zeroconf==0.51.0,
     ],
     extras_require={
         'QRCode': ['pyqrcode', 'base36'],
